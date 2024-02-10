bbded3a4af768acda5196f854e6e0f0ad042faba
landmuc <landmucdev@gmail.com>
2024-02-10 22:05:25 +0100

    SetupViewModel

--- app/src/main/java/com/plcoding/doodlekong/util/Constants.kt
+++ app/src/main/java/com/plcoding/doodlekong/util/Constants.kt
@@ -4,4 +4,10 @@ object Constants {
     const val USE_LOCALHOST = true
     const val HTTP_BASE_URL = ""
     const val HTTP_BASE_URL_LOCALHOST = "http://10.0.2.2:8080/"
+
+    const val MIN_USERNAME_LENGTH = 4
+    const val MAX_USERNAME_LENGTH = 12
+
+    const val MIN_ROOM_NAME_LENGTH = 4
+    const val MAX_ROOM_NAME_LENGTH = 16
 }
