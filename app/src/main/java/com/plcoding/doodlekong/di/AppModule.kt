df911b153184558dce1b90085df854480f27650c
landmuc <landmucdev@gmail.com>
2024-02-11 14:28:22 +0100

    Username Fragment

--- app/src/main/java/com/plcoding/doodlekong/di/AppModule.kt
+++ app/src/main/java/com/plcoding/doodlekong/di/AppModule.kt
@@ -3,6 +3,8 @@ package com.plcoding.doodlekong.di
 import android.content.Context
 import com.google.gson.Gson
 import com.plcoding.doodlekong.data.remote.api.SetupApi
+import com.plcoding.doodlekong.repository.DefaultSetupRepository
+import com.plcoding.doodlekong.repository.SetupRepository
 import com.plcoding.doodlekong.util.Constants.HTTP_BASE_URL
 import com.plcoding.doodlekong.util.Constants.HTTP_BASE_URL_LOCALHOST
 import com.plcoding.doodlekong.util.Constants.USE_LOCALHOST
@@ -26,6 +28,13 @@ import javax.inject.Singleton
 // Binding dependencies to certain lifetimes
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideSetupRepository(
+        setupApi: SetupApi,
+        @ApplicationContext context: Context
+    ): SetupRepository = DefaultSetupRepository(setupApi, context)
 
     @Singleton
     @Provides
